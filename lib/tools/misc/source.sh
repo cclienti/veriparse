d1ba8f63dfe0f57341ccd4731d5600eee6903a6b
Christophe Clienti <cclienti@wavecruncher.net>
2018-04-09 21:19:12 +0200

    fixed anotate_declaration test with localparam0

--- lib/tools/misc/source.sh
+++ lib/tools/misc/source.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function veriparse_meld_yaml {
-   meld_yaml="$HOME/src/veriparse/trunk/lib/tools/misc/meld_yaml.py"
+   meld_yaml="$HOME/src/veriparse/lib/tools/misc/meld_yaml.py"
    $meld_yaml ${1} ${2}
 }
 
 function veriparse_new_ref {
-   yaml_printer="$HOME/src/veriparse/trunk/lib/tools/misc/yaml_printer.py"
+   yaml_printer="$HOME/src/veriparse/lib/tools/misc/yaml_printer.py"
    $yaml_printer ${1} > ${2}
 }
