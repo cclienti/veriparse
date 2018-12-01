9ada1ffe692d78ae49c52c0bc77aa3b797ac122c
Christophe Clienti <cclienti@wavecruncher.net>
2018-12-01 15:02:07 +0100

    [logger] avoid to build string for severity not enabled

--- lib/include/veriparse/logger/logger.hpp
+++ lib/include/veriparse/logger/logger.hpp
@@ -9,20 +9,6 @@
 #include <ostream>
 
 
-namespace Veriparse {
-	namespace Logger {
-		// Forward-declares a global logger with a custom initialization
-		BOOST_LOG_GLOBAL_LOGGER(logger, boost::log::sources::severity_logger_mt<boost::log::trivial::severity_level>)
-
-		std::string get_class_name(const std::string &funcname);
-		void add_stdout_sink(void);
-		void add_stream_sink(std::shared_ptr<std::ostringstream> oss);
-		void add_text_sink(std::string const &filename);
-		void flush(void);
-		void remove_all_sinks(void);
-	}
-}
-
 #ifdef __GNUC__
 #define VERIPARSE_FUNCTION_NAME __PRETTY_FUNCTION__
 #else
@@ -34,34 +20,59 @@ namespace Veriparse {
 	<< "[" << Veriparse::Logger::get_class_name(VERIPARSE_FUNCTION_NAME) << "] "
 
 // Log Macros
-#define LOG_TRACE   VERIPARSE_LOG(trace) // log only in debug
-#define LOG_TRACE_N(_1) if(_1) LOG_TRACE_FL(_1->get_filename(), _1->get_line())
-#define LOG_TRACE_F(_1) VERIPARSE_LOG(trace) << "file " << _1 << ", "
-#define LOG_TRACE_FL(_1, _2) VERIPARSE_LOG(trace) << "file " << _1 << ':' << _2 << ", "
+#define LOG_TRACE            if (Veriparse::Logger::trace_enabled()) VERIPARSE_LOG(trace)
+#define LOG_TRACE_F(_1)      LOG_TRACE << "file " << _1 << ", "
+#define LOG_TRACE_FL(_1, _2) LOG_TRACE << "file " << _1 << ':' << _2 << ", "
+#define LOG_TRACE_N(_1)      if(_1) LOG_TRACE_FL(_1->get_filename(), _1->get_line())
 
-#define LOG_DEBUG   VERIPARSE_LOG(debug) // log only in debug
-#define LOG_DEBUG_N(_1) if(_1) LOG_DEBUG_FL(_1->get_filename(), _1->get_line())
-#define LOG_DEBUG_F(_1) VERIPARSE_LOG(debug) << "file " << _1 << ", "
-#define LOG_DEBUG_FL(_1, _2) VERIPARSE_LOG(debug) << "file " << _1 << ':' << _2 << ", "
+#define LOG_DEBUG            if (Veriparse::Logger::debug_enabled()) VERIPARSE_LOG(debug)
+#define LOG_DEBUG_F(_1)      LOG_DEBUG << "file " << _1 << ", "
+#define LOG_DEBUG_FL(_1, _2) LOG_DEBUG << "file " << _1 << ':' << _2 << ", "
+#define LOG_DEBUG_N(_1)      if(_1) LOG_DEBUG_FL(_1->get_filename(), _1->get_line())
 
-#define LOG_INFO VERIPARSE_LOG(info)
-#define LOG_INFO_N(_1) if(_1) LOG_INFO_FL(_1->get_filename(), _1->get_line())
-#define LOG_INFO_F(_1) VERIPARSE_LOG(info) << "file " << _1 << ", "
-#define LOG_INFO_FL(_1, _2) VERIPARSE_LOG(info) << "file " << _1 << ':' << _2 << ", "
+#define LOG_INFO            if (Veriparse::Logger::info_enabled()) VERIPARSE_LOG(info)
+#define LOG_INFO_F(_1)      LOG_INFO << "file " << _1 << ", "
+#define LOG_INFO_FL(_1, _2) LOG_INFO << "file " << _1 << ':' << _2 << ", "
+#define LOG_INFO_N(_1)      if(_1) LOG_INFO_FL(_1->get_filename(), _1->get_line())
 
-#define LOG_WARNING VERIPARSE_LOG(warning)
-#define LOG_WARNING_N(_1) if(_1) LOG_WARNING_FL(_1->get_filename(), _1->get_line())
-#define LOG_WARNING_F(_1) VERIPARSE_LOG(warning) << "file " << _1 << ", "
-#define LOG_WARNING_FL(_1, _2) VERIPARSE_LOG(warning) << "file " << _1 << ':' << _2 << ", "
+#define LOG_WARNING            if (Veriparse::Logger::warning_enabled()) VERIPARSE_LOG(warning)
+#define LOG_WARNING_F(_1)      LOG_WARNING << "file " << _1 << ", "
+#define LOG_WARNING_FL(_1, _2) LOG_WARNING << "file " << _1 << ':' << _2 << ", "
+#define LOG_WARNING_N(_1)      if(_1) LOG_WARNING_FL(_1->get_filename(), _1->get_line())
 
-#define LOG_ERROR VERIPARSE_LOG(error)
-#define LOG_ERROR_N(_1) if(_1) LOG_ERROR_FL(_1->get_filename(), _1->get_line())
-#define LOG_ERROR_F(_1) VERIPARSE_LOG(error) << "file " << _1 << ", "
-#define LOG_ERROR_FL(_1, _2)   VERIPARSE_LOG(error) << "file " << _1 << ':' << _2 << ", "
+#define LOG_ERROR             VERIPARSE_LOG(error)
+#define LOG_ERROR_F(_1)       LOG_ERROR << "file " << _1 << ", "
+#define LOG_ERROR_FL(_1, _2)  LOG_ERROR << "file " << _1 << ':' << _2 << ", "
+#define LOG_ERROR_N(_1)       if(_1) LOG_ERROR_FL(_1->get_filename(), _1->get_line())
 
-#define LOG_FATAL VERIPARSE_LOG(fatal)
-#define LOG_FATAL_N(_1) if(_1) LOG_FATAL_FL(_1->get_filename(), _1->get_line())
-#define LOG_FATAL_F(_1) VERIPARSE_LOG(fatal) << "file " << _1 << ", "
-#define LOG_FATAL_FL(_1, _2) VERIPARSE_LOG(fatal) << "file " << _1 << ':' << _2 << ", "
+#define LOG_FATAL            VERIPARSE_LOG(fatal)
+#define LOG_FATAL_F(_1)      LOG_FATAL << "file " << _1 << ", "
+#define LOG_FATAL_FL(_1, _2) LOG_FATAL << "file " << _1 << ':' << _2 << ", "
+#define LOG_FATAL_N(_1)      if(_1) LOG_FATAL_FL(_1->get_filename(), _1->get_line())
+
+
+
+namespace Veriparse {
+namespace Logger {
+
+// Forward-declares a global logger with a custom initialization
+BOOST_LOG_GLOBAL_LOGGER(logger, boost::log::sources::severity_logger_mt<boost::log::trivial::severity_level>)
+
+void add_stdout_sink(void);
+void add_stream_sink(std::shared_ptr<std::ostringstream> oss);
+void add_text_sink(std::string const &filename);
+void flush(void);
+void remove_all_sinks(void);
+
+bool warning_enabled(void);
+bool info_enabled(void);
+bool debug_enabled(void);
+bool trace_enabled(void);
+
+std::string get_class_name(const std::string &funcname);
+
+}
+}
+
 
 #endif
--- lib/src/logger/logger.cpp
+++ lib/src/logger/logger.cpp
@@ -18,146 +18,158 @@
 
 
 namespace Veriparse {
-	namespace Logger {
-		std::string get_class_name(const std::string &funcname)
-		{
-			size_t paren = funcname.rfind("(");
-			std::string reduced1 = funcname.substr(0, paren);
+namespace Logger {
 
-			size_t semi1 = reduced1.rfind("::");
-			std::string reduced2 = reduced1.substr(0, semi1);
-
-			size_t semi2 = reduced2.rfind("::");
-			std::string reduced3 = reduced2.substr(semi2+2, reduced2.size()-1);
-
-			size_t semi3 = reduced3.rfind(" ");
-			if (semi3 != std::string::npos) {
-				std::string reduced4 = reduced3.substr(semi3+1, reduced3.size()-1);
-				return reduced4;
-			}
-
-			return reduced3;
-		}
-
-		BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
-		BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", boost::log::trivial::severity_level)
-
-		// specify the format of the log message
-		static boost::log::formatter & logger_get_formatter() {
-			static boost::log::formatter formatter = boost::log::expressions::stream
-				<< boost::log::expressions::format_date_time(timestamp, "[%Y-%m-%d, %H:%M:%S.%f]") << " "
-				<< "[" << boost::log::trivial::severity << "] "
-				<< boost::log::expressions::smessage;
-			return formatter;
-		}
-
-		BOOST_LOG_GLOBAL_LOGGER_INIT(logger, boost::log::sources::severity_logger_mt) {
-			boost::log::sources::severity_logger_mt<boost::log::trivial::severity_level> logger;
-
-			// add attributes
-			logger.add_attribute("TimeStamp", boost::log::attributes::local_clock()); // each log line gets a timestamp
-
-			return logger;
-		}
-
-		// add stdout sink
-		void add_stdout_sink(void) {
-			typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> ostream_sink_t;
-			boost::shared_ptr<ostream_sink_t> ostream_sink = boost::make_shared<ostream_sink_t>();
-			ostream_sink->locked_backend()->add_stream(boost::shared_ptr<std::ostream>
-			                                           (&std::clog, boost::null_deleter()));
-
-			ostream_sink->set_formatter(logger_get_formatter());
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", boost::log::trivial::severity_level)
 
 #ifdef FULL_DEBUG
-			// log all messages
-			ostream_sink->set_filter(severity >= boost::log::trivial::trace);
+// log all messages
+static const auto severity_level = boost::log::trivial::trace;
+#elif DEBUG
+// log debug but not trace
+static const auto severity_level = boost::log::trivial::debug;
 #else
-#ifdef DEBUG
-			ostream_sink->set_filter(severity >= boost::log::trivial::debug);
-#else
-			// log all messages except debug and trace
-			ostream_sink->set_filter(severity >= boost::log::trivial::info);
-#endif
-#endif
-
-			// Enable auto-flushing after each log record written
-			ostream_sink->locked_backend()->auto_flush(true);
-
-			// register our sinks
-			boost::log::core::get()->add_sink(ostream_sink);
-		}
-
-
-		// add stream sink
-		void add_stream_sink(std::shared_ptr<std::ostringstream> oss) {
-			typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> ostream_sink_t;
-			boost::shared_ptr<ostream_sink_t> ostream_sink = boost::make_shared<ostream_sink_t>();
-
-			boost::shared_ptr<std::ostringstream> boss =
-				boost::shared_ptr<std::ostringstream>(oss.get(), [oss](std::ostringstream*) mutable {oss.reset();});
-
-			ostream_sink->locked_backend()->add_stream(boss);
-			ostream_sink->set_formatter(logger_get_formatter());
-
-#ifdef FULL_DEBUG
-			// log all messages
-			ostream_sink->set_filter(severity >= boost::log::trivial::trace);
-#else
-#ifdef DEBUG
-			ostream_sink->set_filter(severity >= boost::log::trivial::debug);
-#else
-			// log all messages except debug and trace
-			ostream_sink->set_filter(severity >= boost::log::trivial::info);
-#endif
+// log all messages except debug and trace
+static const auto severity_level = boost::log::trivial::info;
 #endif
 
-			// Enable auto-flushing after each log record written
-			ostream_sink->locked_backend()->auto_flush(true);
+// specify the format of the log message
+static boost::log::formatter & logger_get_formatter()
+{
+    static boost::log::formatter formatter = boost::log::expressions::stream
+        << boost::log::expressions::format_date_time(timestamp, "[%Y-%m-%d, %H:%M:%S.%f]") << " "
+        << "[" << boost::log::trivial::severity << "] "
+        << boost::log::expressions::smessage;
+    return formatter;
+}
 
-			// register our sinks
-			boost::log::core::get()->add_sink(ostream_sink);
-		}
+BOOST_LOG_GLOBAL_LOGGER_INIT(logger, boost::log::sources::severity_logger_mt)
+{
+    boost::log::sources::severity_logger_mt<boost::log::trivial::severity_level> logger;
 
+    // add attributes
+    logger.add_attribute("TimeStamp", boost::log::attributes::local_clock()); // each log line gets a timestamp
 
-		// add a text file sink
-		void add_text_sink(std::string const &filename) {
-			typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_file_backend> file_sink_t;
-			boost::shared_ptr<boost::log::sinks::text_file_backend> file_backend =
-				boost::make_shared<boost::log::sinks::text_file_backend>
-				(boost::log::keywords::file_name = filename.c_str(),
-				 boost::log::keywords::rotation_size = 10 * 1024 * 1024);
-			boost::shared_ptr<file_sink_t> file_sink = boost::make_shared<file_sink_t>(file_backend);
+    return logger;
+}
 
-			file_sink->set_formatter(logger_get_formatter());
+// add stdout sink
+void add_stdout_sink(void)
+{
+    typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> ostream_sink_t;
+    boost::shared_ptr<ostream_sink_t> ostream_sink = boost::make_shared<ostream_sink_t>();
+    ostream_sink->locked_backend()->add_stream(boost::shared_ptr<std::ostream>
+                                               (&std::clog, boost::null_deleter()));
 
-#ifdef FULL_DEBUG
-			// log all messages
-			file_sink->set_filter(severity >= boost::log::trivial::trace);
-#else
-#ifdef DEBUG
-			file_sink->set_filter(severity >= boost::log::trivial::debug);
-#else
-			// log all messages except debug and trace
-			file_sink->set_filter(severity >= boost::log::trivial::info);
-#endif
-#endif
+    ostream_sink->set_formatter(logger_get_formatter());
 
-			// Enable auto-flushing after each log record written
-			file_backend->auto_flush(true);
+    // Set debug level
+    ostream_sink->set_filter(severity >= severity_level);
 
-			boost::log::core::get()->add_sink(file_sink);
-		}
+    // Enable auto-flushing after each log record written
+    ostream_sink->locked_backend()->auto_flush(true);
 
-		void flush(void) {
-			boost::log::core::get()->flush();
-		}
-
-		void remove_all_sinks(void) {
-			flush();
-			boost::log::core::get()->remove_all_sinks();
-		}
+    // register our sinks
+    boost::log::core::get()->add_sink(ostream_sink);
+}
 
 
-	}
+// add stream sink
+void add_stream_sink(std::shared_ptr<std::ostringstream> oss)
+{
+    typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> ostream_sink_t;
+    boost::shared_ptr<ostream_sink_t> ostream_sink = boost::make_shared<ostream_sink_t>();
+
+    boost::shared_ptr<std::ostringstream> boss =
+        boost::shared_ptr<std::ostringstream>(oss.get(), [oss](std::ostringstream*) mutable {oss.reset();});
+
+    ostream_sink->locked_backend()->add_stream(boss);
+    ostream_sink->set_formatter(logger_get_formatter());
+
+    // Set debug level
+    ostream_sink->set_filter(severity >= severity_level);
+
+    // Enable auto-flushing after each log record written
+    ostream_sink->locked_backend()->auto_flush(true);
+
+    // register our sinks
+    boost::log::core::get()->add_sink(ostream_sink);
+}
+
+
+// add a text file sink
+void add_text_sink(std::string const &filename)
+{
+    typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_file_backend> file_sink_t;
+    boost::shared_ptr<boost::log::sinks::text_file_backend> file_backend =
+        boost::make_shared<boost::log::sinks::text_file_backend>
+        (boost::log::keywords::file_name = filename.c_str(),
+         boost::log::keywords::rotation_size = 10 * 1024 * 1024);
+    boost::shared_ptr<file_sink_t> file_sink = boost::make_shared<file_sink_t>(file_backend);
+
+    file_sink->set_formatter(logger_get_formatter());
+
+    // Set debug level
+    file_sink->set_filter(severity >= severity_level);
+
+    // Enable auto-flushing after each log record written
+    file_backend->auto_flush(true);
+
+    boost::log::core::get()->add_sink(file_sink);
+}
+
+void flush(void)
+{
+    boost::log::core::get()->flush();
+}
+
+void remove_all_sinks(void)
+{
+    flush();
+    boost::log::core::get()->remove_all_sinks();
+}
+
+bool warning_enabled(void)
+{
+    return boost::log::trivial::warning >= severity_level;
+}
+
+bool info_enabled(void)
+{
+    return boost::log::trivial::info >= severity_level;
+}
+
+bool debug_enabled(void)
+{
+    return boost::log::trivial::debug >= severity_level;
+}
+
+bool trace_enabled(void)
+{
+    return boost::log::trivial::trace >= severity_level;
+}
+
+std::string get_class_name(const std::string &funcname)
+{
+    size_t paren = funcname.rfind("(");
+    std::string reduced1 = funcname.substr(0, paren);
+
+    size_t semi1 = reduced1.rfind("::");
+    std::string reduced2 = reduced1.substr(0, semi1);
+
+    size_t semi2 = reduced2.rfind("::");
+    std::string reduced3 = reduced2.substr(semi2+2, reduced2.size()-1);
+
+    size_t semi3 = reduced3.rfind(" ");
+    if (semi3 != std::string::npos) {
+        std::string reduced4 = reduced3.substr(semi3+1, reduced3.size()-1);
+        return reduced4;
+    }
+
+    return reduced3;
+}
+
+
+}
 }
