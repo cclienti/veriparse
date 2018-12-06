a1e5e2b981dc6a2662340fd90c26561e252258fe
Christophe Clienti <cclienti@wavecruncher.net>
2018-12-06 23:22:54 +0100

    [passes] fixed namespace indentation in instance analysis

--- lib/include/veriparse/passes/analysis/instance.hpp
+++ lib/include/veriparse/passes/analysis/instance.hpp
@@ -11,37 +11,37 @@
 
 
 namespace Veriparse {
-	namespace Passes {
-		namespace Analysis {
+namespace Passes {
+namespace Analysis {
 
-			class Instance: public StandardSearch {
-			public:
-				Instance() = delete;
+class Instance: public StandardSearch {
+public:
+    Instance() = delete;
 
-				/**
-				 * Return identifier nodes from an instance
-				 */
-				static AST::Identifier::ListPtr get_argument_identifier_nodes(AST::Instance::Ptr instance);
+    /**
+     * Return identifier nodes from an instance
+     */
+    static AST::Identifier::ListPtr get_argument_identifier_nodes(AST::Instance::Ptr instance);
 
-				/**
-				 * Return identifier names from an instance
-				 */
-				static std::vector<std::string> get_argument_identifier_names(AST::Instance::Ptr instance);
+    /**
+     * Return identifier names from an instance
+     */
+    static std::vector<std::string> get_argument_identifier_names(AST::Instance::Ptr instance);
 
-				/**
-				 * Return identifier nodes from an instance
-				 */
-				static AST::PortArg::ListPtr get_port_argument_nodes(AST::Instance::Ptr instance);
+    /**
+     * Return identifier nodes from an instance
+     */
+    static AST::PortArg::ListPtr get_port_argument_nodes(AST::Instance::Ptr instance);
 
-				/**
-				 * Return identifier names from an instance
-				 */
-				static std::vector<std::string> get_port_argument_names(AST::Instance::Ptr instance);
+    /**
+     * Return identifier names from an instance
+     */
+    static std::vector<std::string> get_port_argument_names(AST::Instance::Ptr instance);
 
-			};
+};
 
-		}
-	}
+}
+}
 }
 
 #endif
--- lib/src/passes/analysis/instance.cpp
+++ lib/src/passes/analysis/instance.cpp
@@ -4,33 +4,33 @@
 
 
 namespace Veriparse {
-	namespace Passes {
-		namespace Analysis {
+namespace Passes {
+namespace Analysis {
 
-			AST::Identifier::ListPtr Instance::get_argument_identifier_nodes(AST::Instance::Ptr instance) {
-				auto list = std::make_shared<AST::Identifier::List>();
-				get_node_list<AST::Identifier>(instance, AST::NodeType::Identifier, list);
-				return list;
-			}
+AST::Identifier::ListPtr Instance::get_argument_identifier_nodes(AST::Instance::Ptr instance) {
+    auto list = std::make_shared<AST::Identifier::List>();
+    get_node_list<AST::Identifier>(instance, AST::NodeType::Identifier, list);
+    return list;
+}
 
-			std::vector<std::string> Instance::get_argument_identifier_names(AST::Instance::Ptr instance) {
-				auto identifiers = get_argument_identifier_nodes(instance);
-				return get_property_in_list<std::string, AST::Identifier>
-					(identifiers, [](AST::Identifier::Ptr n){return n->get_name();});
-			}
+std::vector<std::string> Instance::get_argument_identifier_names(AST::Instance::Ptr instance) {
+    auto identifiers = get_argument_identifier_nodes(instance);
+    return get_property_in_list<std::string, AST::Identifier>
+        (identifiers, [](AST::Identifier::Ptr n){return n->get_name();});
+}
 
-			AST::PortArg::ListPtr Instance::get_port_argument_nodes(AST::Instance::Ptr instance) {
-				auto list = std::make_shared<AST::PortArg::List>();
-				get_node_list<AST::PortArg>(instance, AST::NodeType::PortArg, list);
-				return list;
-			}
+AST::PortArg::ListPtr Instance::get_port_argument_nodes(AST::Instance::Ptr instance) {
+    auto list = std::make_shared<AST::PortArg::List>();
+    get_node_list<AST::PortArg>(instance, AST::NodeType::PortArg, list);
+    return list;
+}
 
-			std::vector<std::string> Instance::get_port_argument_names(AST::Instance::Ptr instance) {
-				auto identifiers = get_port_argument_nodes(instance);
-				return get_property_in_list<std::string, AST::PortArg>
-					(identifiers, [](AST::PortArg::Ptr n){return n->get_name();});
-			}
+std::vector<std::string> Instance::get_port_argument_names(AST::Instance::Ptr instance) {
+    auto identifiers = get_port_argument_nodes(instance);
+    return get_property_in_list<std::string, AST::PortArg>
+        (identifiers, [](AST::PortArg::Ptr n){return n->get_name();});
+}
 
-		}
-	}
+}
+}
 }
