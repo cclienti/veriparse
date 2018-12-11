144a4b81a6344189c7d58044f345dc0909f79902
Christophe Clienti <cclienti@wavecruncher.net>
2018-12-11 07:23:13 +0100

    [tests] validated flattener with an added testbench

--- lib/test/passes/transformations/testcases/instance4.v
+++ lib/test/passes/transformations/testcases/instance4.v
@@ -23,3 +23,40 @@ module dffn (q, d, clk);
 
    DFF dff[2:0] (q, d, clk);
 endmodule
+
+
+module testbench;
+
+   reg [2:0]  in0;
+   reg [2:0]  in1;
+   wire [2:0] out0;
+   wire [2:0] out1;
+
+   reg  clk = 0;
+   always
+     #5 clk = ~clk;
+
+   instance4 instance4_inst
+   (
+      .in0  (in0),
+      .in1  (in1),
+      .out0 (out0),
+      .out1 (out1),
+      .clk  (clk)
+   );
+
+   integer cpt = 0;
+
+   always @(posedge clk) begin
+      cpt <= cpt + 1;
+      in0 <= cpt[2:0];
+      in1 <= cpt[5:3];
+      if (cpt == 15)
+        $finish;
+   end
+
+   always @(clk) begin
+      $display(in0, in1, out0, out1);
+   end
+
+endmodule
