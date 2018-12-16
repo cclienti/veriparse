8d13d6685133309a52b9da87dbb0b6f9a25be9a6
Christophe Clienti <cclienti@wavecruncher.net>
2018-12-16 22:48:37 +0100

    added a global cmake list file
    Added also to the cmake common list file a guard to prevent multiple
    inclusions.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,67 +1,73 @@
-cmake_minimum_required (VERSION 3.7)
-message(STATUS "Using the veriparse common cmake listfile")
-include(ExternalProject)
+cmake_minimum_required (VERSION 3.9)
 
-########################################
-### Custom Modules
-########################################
+if(NOT DEFINED VERIPARSE_COMMON_CMAKE)
+  set(VERIPARSE_COMMON_CMAKE TRUE)
 
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_LIST_DIR}/../cmake/modules")
+  message(STATUS "Using the veriparse common cmake listfile")
+  include(ExternalProject)
+
+  ########################################
+  ### Custom Modules
+  ########################################
+
+  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_LIST_DIR}/../cmake/modules")
 
 
-########################################
-### CCache
-########################################
+  ########################################
+  ### CCache
+  ########################################
 
-option(ENABLE_CCACHE "Use ccache" OFF)
+  option(ENABLE_CCACHE "Use ccache" OFF)
 
-if(ENABLE_CCACHE)
+  if(ENABLE_CCACHE)
     find_program(CCACHE_FOUND ccache)
     if(CCACHE_FOUND)
-	message(STATUS "Using CCache")
-	set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
-	set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+	  message(STATUS "Using CCache")
+	  set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+	  set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
     endif(CCACHE_FOUND)
+  endif()
+
+
+  ########################################
+  ### Compiler flags
+  ########################################
+
+  set(CMAKE_CXX_FLAGS "-std=c++14 -Wall")
+  set(CMAKE_CXX_FLAGS_DEBUG  "-g -DDEBUG")
+  set(CMAKE_CXX_FLAGS_FULL_DEBUG  "-g3 -DDEBUG -DFULL_DEBUG")
+  set(CMAKE_CXX_FLAGS_RELEASE "-Werror -g -O3")
+  set(CMAKE_CXX_FLAGS_COVERAGE "-g3 -DDEBUG -fprofile-arcs -ftest-coverage")
+
+
+  ########################################
+  ### GTest
+  ########################################
+
+  include(external-googletest)
+
+
+  ########################################
+  ### YAML-CPP
+  ########################################
+
+  include(external-yaml-cpp)
+
+
+  ########################################
+  ### Boost configuration
+  ########################################
+
+  set(BOOST_MODULES
+	system program_options log date_time
+	log_setup filesystem thread regex chrono atomic)
+  include(external-boost)
+
+
+  ########################################
+  ### GMP
+  ########################################
+
+  include(external-gmp)
+
 endif()
-
-
-########################################
-### Compiler flags
-########################################
-
-set(CMAKE_CXX_FLAGS "-std=c++14 -Wall")
-set(CMAKE_CXX_FLAGS_DEBUG  "-g -DDEBUG")
-set(CMAKE_CXX_FLAGS_FULL_DEBUG  "-g3 -DDEBUG -DFULL_DEBUG")
-set(CMAKE_CXX_FLAGS_RELEASE "-Werror -g -O3")
-set(CMAKE_CXX_FLAGS_COVERAGE "-g3 -DDEBUG -fprofile-arcs -ftest-coverage")
-
-
-########################################
-### GTest
-########################################
-
-include(external-googletest)
-
-
-########################################
-### YAML-CPP
-########################################
-
-include(external-yaml-cpp)
-
-
-########################################
-### Boost configuration
-########################################
-
-set(BOOST_MODULES
-  system program_options log date_time
-  log_setup filesystem thread regex chrono atomic)
-include(external-boost)
-
-
-########################################
-### GMP
-########################################
-
-include(external-gmp)
