a9ade60953a9ab2010165f633fb46ac536be8958
Christophe Clienti <cclienti@wavecruncher.net>
2019-01-21 22:12:44 +0100

    [license] added a cmake custom target to test licence_validator

--- apps/license_validator/check.sh
+++ apps/license_validator/check.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+if [[ "$#" != "3" ]]; then
+    echo "usage: check.sh <license_generator_path> <license_validator_path> <pivate_key>"
+    exit 1
+fi
+
 #------------------------------------------------------
 # Where to find the license programs
 #------------------------------------------------------
 
-PATH=apps/license_generator:apps/license_validator:$PATH
-
+LICENSE_GENERATOR=$1
+LICENSE_VALIDATOR=$2
 
 #------------------------------------------------------
 # RSA test keys
 #------------------------------------------------------
 
-PRIVATE_KEY=$(git rev-parse --show-toplevel)/apps/license_generator/resources/private.pem
+PRIVATE_KEY=$3  # $(git rev-parse --show-toplevel)/apps/license_generator/resources/private.pem
 
 
 #------------------------------------------------------
@@ -35,8 +40,8 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
-license_validator license_test.dat || exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_VALIDATOR license_test.dat || exit 1
 
 
 #------------------------------------------------------
@@ -52,8 +57,8 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
-license_validator license_test.dat || exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_VALIDATOR license_test.dat || exit 1
 
 
 #------------------------------------------------------
@@ -69,8 +74,8 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
-license_validator license_test.dat || exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_VALIDATOR license_test.dat || exit 1
 
 
 #------------------------------------------------------
@@ -86,8 +91,8 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
-license_validator license_test.dat && exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_VALIDATOR license_test.dat && exit 1
 
 
 #------------------------------------------------------
@@ -103,9 +108,9 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
 sed -i 's/company = .*$/company = Other/g' license_test.dat
-license_validator license_test.dat && exit 1
+$LICENSE_VALIDATOR license_test.dat && exit 1
 
 
 #------------------------------------------------------
@@ -121,9 +126,9 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
 sed -i 's/expiration = .*$/expiration = 2040-12-12/g' license_test.dat
-license_validator license_test.dat && exit 1
+$LICENSE_VALIDATOR license_test.dat && exit 1
 
 
 #------------------------------------------------------
@@ -139,9 +144,9 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
 sed -i 's/hostname = .*$/hostname = Other/g' license_test.dat
-license_validator license_test.dat && exit 1
+$LICENSE_VALIDATOR license_test.dat && exit 1
 
 
 #------------------------------------------------------
@@ -157,9 +162,9 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
 sed -i 's/interface = .*$/interface = other42/g' license_test.dat
-license_validator license_test.dat && exit 1
+$LICENSE_VALIDATOR license_test.dat && exit 1
 
 
 #------------------------------------------------------
@@ -175,9 +180,9 @@ interface = $INTERFACE
 address = $MAC_ADDRESS
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
 sed -i 's/address = .*$/address = 00:11:22:33:44:55/g' license_test.dat
-license_validator license_test.dat && exit 1
+$LICENSE_VALIDATOR license_test.dat && exit 1
 
 
 #------------------------------------------------------
@@ -193,8 +198,8 @@ interface = $INTERFACE
 address = 00:11:22:33:44:55
 EOF
 
-license_generator --private $PRIVATE_KEY license_test.ini || exit 1
-license_validator license_test.dat && exit 1
+$LICENSE_GENERATOR --private $PRIVATE_KEY license_test.ini || exit 1
+$LICENSE_VALIDATOR license_test.dat && exit 1
 
 
 #------------------------------------------------------
