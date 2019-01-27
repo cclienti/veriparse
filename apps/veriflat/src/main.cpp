96276bc2201adc5c41e7c33f4619c54b7f4c0bef
Christophe Clienti <cclienti@wavecruncher.net>
2019-01-27 22:35:43 +0100

    [apps] added code to flatten a module

--- apps/veriflat/src/main.cpp
+++ apps/veriflat/src/main.cpp
@@ -1,14 +1,52 @@
 #include <license/license_checker.hpp>
 
 #include <veriparse/logger/logger.hpp>
+#include <veriparse/parser/verilog.hpp>
+#include <veriparse/generators/verilog_generator.hpp>
+#include <veriparse/passes/analysis/module.hpp>
+#include <veriparse/passes/analysis/unique_declaration.hpp>
+#include <veriparse/passes/transformations/module_flattener.hpp>
+
+#include <yaml-cpp/yaml.h>
 
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 
 #include <string>
+#include <ostream>
 
 
-void show_usage(char const * const progname, boost::program_options::options_description const &desc)
+struct Config
+{
+	std::vector<std::string> inputs;
+	std::string output;
+	std::string top_module;
+	std::string param_map;
+	bool obfuscate {false};
+};
+
+static std::ostream &operator<<(std::ostream &os, const Config &config)
+{
+	os << "{inputs: [";
+	std::stringstream ss;
+	for (int i = config.inputs.size()-1; i >= 0; i--) {
+		ss << "'" << config.inputs[i] << "'";
+		if (i != 0) {
+			ss << ", ";
+		}
+	}
+	os << ss.str() << "], ";
+
+	os << "output: '" << config.output << "', ";
+	os << "top_module: '" << config.top_module << "', ";
+	os << "param_map: " << config.param_map << ", ";
+	os << "obfuscate: " << config.obfuscate;
+	os << "}";
+
+	return os;
+}
+
+static void show_usage(char const * const progname, boost::program_options::options_description const &desc)
 {
 	boost::filesystem::path p(progname);
 	std::cout << "Usage: " << p.filename().string() << " [options] verilog-file [verilog-file ...]"
@@ -16,17 +54,27 @@ void show_usage(char const * const progname, boost::program_options::options_des
 	std::cout << desc << std::endl;
 }
 
-struct Config
-{
-	std::vector<std::string> inputs;
-	std::string output;
-};
-
 static int veriflat(int argc, char *argv[])
 {
+	//---------------------------------------------------------
+	// Prepare logger
+	//---------------------------------------------------------
+
 	Veriparse::Logger::remove_all_sinks();
 	Veriparse::Logger::add_text_sink("veriflat.log");
 	Veriparse::Logger::add_stdout_sink();
+
+
+	//---------------------------------------------------------
+	// Set seed
+	//---------------------------------------------------------
+
+	Veriparse::Passes::Analysis::UniqueDeclaration::seed(0);
+
+
+	//---------------------------------------------------------
+	// Parse command line
+	//---------------------------------------------------------
 
 	Config config;
 
@@ -34,6 +82,9 @@ static int veriflat(int argc, char *argv[])
 	options.add_options()
 		("help,h", "Produce help message")
 		("output,o", boost::program_options::value<std::string>(&config.output)->required(), "output")
+		("top-module,t", boost::program_options::value<std::string>(&config.top_module)->required(), "top-module")
+		("param-map,p", boost::program_options::value<std::string>(&config.param_map), "YAML parameter map")
+		("obfuscate,b", boost::program_options::bool_switch(&config.obfuscate), "obfuscate")
 		;
 
 	boost::program_options::options_description hidden("positional");
@@ -76,6 +127,115 @@ static int veriflat(int argc, char *argv[])
 		LOG_ERROR << "missing verilog file(s)";
 		show_usage(argv[0], desc);
 	}
+
+	LOG_INFO << "Command line: " << config;
+
+
+	//---------------------------------------------------------
+	// Create a map of all modules
+	//---------------------------------------------------------
+
+	Veriparse::Passes::Analysis::Module::ModulesMap modules_map;
+	for (const auto &input: config.inputs) {
+		Veriparse::Parser::Verilog verilog;
+		verilog.parse(input);
+		auto source = verilog.get_source();
+		Veriparse::Passes::Analysis::Module::get_module_dictionary(source, modules_map);
+	}
+
+	if (modules_map.count(config.top_module) == 0) {
+		LOG_ERROR << "module " << config.top_module << " not found";
+		return 1;
+	}
+
+	auto module = modules_map[config.top_module];
+
+
+	//---------------------------------------------------------
+	// Prepare parameter overloading
+	//---------------------------------------------------------
+
+	YAML::Node node;
+
+	try {
+		node = YAML::Load(config.param_map);
+	}
+	catch ( const std::exception & e ) {
+		LOG_ERROR << "could not read parameters from command line";
+		return 1;
+	}
+
+	Veriparse::AST::ParamArg::ListPtr paramargs;
+
+	if (!node.IsNull()) {
+		if (node.IsMap()) {
+			std::stringstream ssparams;
+			ssparams << "module params;";
+			for (auto param: node) {
+				if (param.second.IsScalar()) {
+					ssparams << "parameter " << param.first << " = " << param.second << ";";
+				}
+				else {
+					LOG_ERROR << "Command line parameter " << param.first << " is not scalar";
+					return 1;
+				}
+			}
+			ssparams << "endmodule";
+
+			Veriparse::Parser::Verilog verilog;
+			verilog.parse(ssparams);
+			auto params = Veriparse::Passes::Analysis::Module::get_parameter_nodes(verilog.get_source());
+
+			if (params && params->size() != 0) {
+				paramargs = std::make_shared<Veriparse::AST::ParamArg::List>();
+				for (auto param: *params) {
+					auto name = param->get_name();
+					auto rvalue = param->get_value();
+
+					if (rvalue) {
+						auto value = Veriparse::AST::cast_to<Veriparse::AST::Rvalue>(rvalue);
+						LOG_INFO << "Overloading parameter: " << name << " = "
+						         << Veriparse::Generators::VerilogGenerator().render(value);
+						paramargs->push_back(std::make_shared<Veriparse::AST::ParamArg>(value, name));
+					}
+					else {
+						LOG_ERROR << "Value of overloaded parameter " << name << " is malformed";
+						return 1;
+					}
+				}
+			}
+		}
+		else {
+			LOG_ERROR << "Parameters from the command line are not in a map";
+			return 1;
+		}
+	}
+
+
+	//---------------------------------------------------------
+	// Flatten the selected module
+	//---------------------------------------------------------
+
+	Veriparse::Passes::Transformations::ModuleFlattener flattener(paramargs, modules_map);
+	flattener.run(module);
+
+
+	//---------------------------------------------------------
+	// Flatten the selected module
+	//---------------------------------------------------------
+
+	if (config.obfuscate) {
+		LOG_WARNING << "Obfuscator not implemented";
+	}
+
+
+	//---------------------------------------------------------
+	// Write the result into the output file
+	//---------------------------------------------------------
+
+	const std::string str = Veriparse::Generators::VerilogGenerator().render(module);
+	std::ofstream fout(config.output);
+	fout << str << std::endl;
 
 	return 0;
 }
