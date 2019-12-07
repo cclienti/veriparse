0822e72e5b2000c897a54fc296d5b49618c5e266
Christophe Clienti <cclienti@wavecruncher.net>
2019-12-07 14:06:37 +0100

    [cmake] fix boost compilation with python3

--- external/external-boost.cmake
+++ external/external-boost.cmake
@@ -6,15 +6,21 @@ endif()
 set(BOOST_URL_PREFIX_DIR ${CMAKE_CURRENT_BINARY_DIR}/external/boost)
 set(BOOST_URL_INSTALL_DIR ${BOOST_URL_PREFIX_DIR}/install)
 
+execute_process(COMMAND python3 -c "import sys; sys.stdout.write('{}.{}'.format(sys.version_info.major, sys.version_info.minor))"
+                OUTPUT_VARIABLE PYTHON_VERSION)
+
+message(STATUS "Python Major Version: ${PYTHON_VERSION}")
 message(STATUS "External project Boost installation in ${BOOST_URL_INSTALL_DIR}")
 
 set(BOOST_URL            https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.bz2)
 set(BOOST_SHA256         430ae8354789de4fd19ee52f3b1f739e1fba576f0aded0897c3c2bc00fb38778)
 
 # Patch command is used to setup the install
-set(BOOST_PATCH_CMD      echo "using gcc : : ${CMAKE_CXX_COMPILER} : <cxxflags>-Wno-deprecated-declarations $<SEMICOLON>" > tools/build/src/user-config.jam)
+set(BOOST_PATCH_CMD
+  echo "using gcc : : ${CMAKE_CXX_COMPILER} : <cxxflags>-Wno-deprecated-declarations $<SEMICOLON>"  > tools/build/src/user-config.jam &&
+  echo "using python : ${PYTHON_VERSION} : /usr/bin/python${PYTHON_VERSION} : /usr/include/python${PYTHON_VERSION}m : /usr/lib $<SEMICOLON>" >> tools/build/src/user-config.jam)
 set(BOOST_BOOTSTRAP_CMD  ./bootstrap.sh --prefix=${BOOST_URL_INSTALL_DIR})
-set(BOOST_BUILD_CMD      ./b2 -j${VERIPARSE_PROCESSOR_COUNT} toolset=gcc link=static threading=multi variant=release install)
+set(BOOST_BUILD_CMD      ./b2 -j${VERIPARSE_PROCESSOR_COUNT} python=${PYTHON_VERSION} toolset=gcc link=static threading=multi variant=release install)
 
 # Boost patch if needed (boost 1.64)
 # Installed command is used to patch boost install against -Werror=parentheses
