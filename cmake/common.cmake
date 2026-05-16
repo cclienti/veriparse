0eb5ffa6c2f051105df5af16000e21818a89298e
Christophe Clienti <cclienti@wavecruncher.net>
2026-05-16 14:04:09 +0200

    fix: create GMP imported targets for proper dependency propagation

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -94,23 +94,29 @@ if(NOT DEFINED VERIPARSE_COMMON_CMAKE)
   ### GMP
   ########################################
 
-  find_path(GMP_INCLUDE_DIR
-	NAMES gmp.h)
-
-  find_library(GMP_LIBRARY
-	NAMES gmp)
-
+  find_path(GMP_INCLUDE_DIR NAMES gmp.h)
+  find_library(GMP_LIBRARY NAMES gmp)
   find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARY)
   mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARY)
 
-  find_path(GMPXX_INCLUDE_DIR
-	NAMES gmpxx.h)
+  if(GMP_FOUND AND NOT TARGET GMP::GMP)
+    add_library(GMP::GMP UNKNOWN IMPORTED)
+    set_target_properties(GMP::GMP PROPERTIES
+      IMPORTED_LOCATION "${GMP_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${GMP_INCLUDE_DIR}")
+  endif()
 
-  find_library(GMPXX_LIBRARY
-	NAMES gmpxx)
-
+  find_path(GMPXX_INCLUDE_DIR NAMES gmpxx.h)
+  find_library(GMPXX_LIBRARY NAMES gmpxx)
   find_package_handle_standard_args(GMPXX DEFAULT_MSG GMPXX_INCLUDE_DIR GMPXX_LIBRARY)
   mark_as_advanced(GMPXX_INCLUDE_DIR GMPXX_LIBRARY)
+
+  if(GMPXX_FOUND AND NOT TARGET GMP::GMPXX)
+    add_library(GMP::GMPXX UNKNOWN IMPORTED)
+    set_target_properties(GMP::GMPXX PROPERTIES
+      IMPORTED_LOCATION "${GMPXX_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${GMPXX_INCLUDE_DIR}")
+  endif()
 
   ########################################
   ### RPATH configuration
