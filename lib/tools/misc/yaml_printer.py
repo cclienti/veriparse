a4f80353a6ccc1e9b582ceacc7a725f518d725a4
Christophe Clienti <cclienti@wavecruncher.net>
2026-05-17 09:44:03 +0200

    style: pretty-print all YAML test reference files

--- lib/tools/misc/yaml_printer.py
+++ lib/tools/misc/yaml_printer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2013-2026 Christophe Clienti
 """Beautify a YAML file"""
@@ -11,8 +11,8 @@ import yaml
 def main():
     """Main procedure"""
 
-    parser = argparse.ArgumentParser(description='Indent a YAML file or stdin')
-    parser.add_argument('infile', type=str, nargs='?', help='YAML file')
+    parser = argparse.ArgumentParser(description="Indent a YAML file or stdin")
+    parser.add_argument("infile", type=str, nargs="?", help="YAML file")
 
     args = parser.parse_args()
 
@@ -25,9 +25,14 @@ def main():
             sys.stderr.write("Cannot open file '{}'\n".format(args.infile))
             sys.exit(1)
 
-    print(yaml.dump(yaml.load(yamlstr, Loader=yaml.Loader),
-                    default_flow_style=False, Dumper=yaml.Dumper))
+    print(
+        yaml.dump(
+            yaml.load(yamlstr, Loader=yaml.Loader),
+            default_flow_style=False,
+            Dumper=yaml.Dumper,
+        )
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
