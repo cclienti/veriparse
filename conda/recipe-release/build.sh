3e1a51a2010c0d76db92f74bd7a15f6ce0078e24
Christophe Clienti <cclienti@wavecruncher.net>
2026-05-17 17:10:28 +0200

    ci: restrict mold linker flags to linux only in build.sh

--- conda/recipe-release/build.sh
+++ conda/recipe-release/build.sh
@@ -6,17 +6,24 @@ rm -rf build
 mkdir -p build
 cd build
 
+# mold linker is only supported on Linux
+if [[ "${target_platform}" == linux-* ]]; then
+  MOLD_FLAGS="-DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=mold -DCMAKE_SHARED_LINKER_FLAGS=-fuse-ld=mold"
+else
+  MOLD_FLAGS=""
+fi
+
 cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=mold" \
-  -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=mold" \
   -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+  ${MOLD_FLAGS} \
   ${SRC_DIR}
 
-VERIPARSE_SOURCE_ROOT=${SRC_DIR} make ARGS="-j${CPU_COUNT} -L unittest"  test
+make -j ${CPU_COUNT}
 
+VERIPARSE_SOURCE_ROOT=${SRC_DIR} make ARGS="-j${CPU_COUNT} -L unittest" test
 
 make install
